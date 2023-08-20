c449ddad8266c7fc88453f76a24c379af2896ac0
Sathyanarayanan-R <rsathyajana2000@gmail.com>
2023-08-20 12:39:02 +0530

    order routes

---
+++ routes/api/order.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const router = express.Router();
+
+// Bring in Models & Utils
+const Order = require('../../models/order');
+const Cart = require('../../models/cart');
+const auth = require('../../middleware/auth');
+const store = require('../../utils/store');
+const { ROLES } = require('../../constants');
+
+router.post('/add', auth, async (req, res) => {
+  try {
+    const cart = req.body.cartId;
+    const total = req.body.total;
+    const user = req.user._id;
+
+    const order = new Order({
+      cart,
+      user,
+      total
+    });
+
+    const orderDoc = await order.save();
+
+    const cartDoc = await Cart.findById(orderDoc.cart._id).populate({
+      path: 'products.product',
+      populate: {
+        path: 'brand'
+      }
+    });
+
+    const newOrder = {
+      _id: orderDoc._id,
+      created: orderDoc.created,
+      user: orderDoc.user,
+      total: orderDoc.total,
+      products: cartDoc.products
+    };
+
+    res.status(200).json({
+      success: true,
+      message: `Your order has been placed successfully!`,
+      order: { _id: orderDoc._id }
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+// fetch my orders api
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = req.user._id;
+    const query = { user };
+
+    const ordersDoc = await Order.find(query)
+      .sort('-created')
+      .populate({
+        path: 'cart',
+        populate: {
+          path: 'products.product',
+          populate: {
+            path: 'brand'
+          }
+        }
+      }).exec();
+
+    const count = await Order.countDocuments(query);
+    const orders = store.formatOrders(ordersDoc);
+
+    res.status(200).json({
+      orders,
+      count
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+// fetch order api
+router.get('/:orderId', auth, async (req, res) => {
+  try {
+    const orderId = req.params.orderId;
+
+    let orderDoc = null;
+
+    if (req.user.role === ROLES.Admin) {
+      orderDoc = await Order.findOne({ _id: orderId }).populate({
+        path: 'cart',
+        populate: {
+          path: 'products.product',
+          populate: {
+            path: 'brand'
+          }
+        }
+      });
+    } else {
+      const user = req.user._id;
+      orderDoc = await Order.findOne({ _id: orderId, user }).populate({
+        path: 'cart',
+        populate: {
+          path: 'products.product',
+          populate: {
+            path: 'brand'
+          }
+        }
+      });
+    }
+
+    if (!orderDoc || !orderDoc.cart) {
+      return res.status(404).json({
+        message: `Cannot find order with the id: ${orderId}.`
+      });
+    }
+
+    let order = {
+      _id: orderDoc._id,
+      total: orderDoc.total,
+      created: orderDoc.created,
+      totalTax: 0,
+      products: orderDoc?.cart?.products,
+      cartId: orderDoc.cart._id
+    };
+
+    order = store.caculateTaxAmount(order);
+
+    res.status(200).json({
+      order
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+module.exports = router;
