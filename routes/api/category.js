1b2366311192c245de419b6cf191440b761a7078
Sathyanarayanan-R <rsathyajana2000@gmail.com>
2023-08-20 12:32:38 +0530

    category routes

---
+++ routes/api/category.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+
+// Bring in Models & Utils
+const Category = require('../../models/category');
+const auth = require('../../middleware/auth');
+const role = require('../../middleware/role');
+const { ROLES } = require('../../constants');
+
+router.post('/add', auth, role.check(ROLES.Admin), (req, res) => {
+  const name = req.body.name;
+  const description = req.body.description;
+  const products = req.body.products;
+  const isActive = req.body.isActive;
+
+  if (!description || !name) {
+    return res
+      .status(400)
+      .json({ error: 'You must enter description & name.' });
+  }
+
+  const category = new Category({
+    name,
+    description,
+    products,
+    isActive
+  });
+
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Your request could not be processed. Please try again.'
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Category has been added successfully!`,
+      category: data
+    });
+  });
+});
+
+// fetch categories api
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.find({});
+    res.status(200).json({
+      categories
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+// fetch category api
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+
+    const categoryDoc = await Category.findOne({ _id: categoryId }).populate({
+      path: 'products',
+      select: 'name'
+    });
+
+    if (!categoryDoc) {
+      return res.status(404).json({
+        message: 'No Category found.'
+      });
+    }
+
+    res.status(200).json({
+      category: categoryDoc
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+module.exports = router;
