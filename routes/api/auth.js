9036df819e5fc7d38f0f2c08bf644664cf6bbae1
Sathyanarayanan-R <rsathyajana2000@gmail.com>
2023-08-20 12:27:23 +0530

    auth routes

---
+++ routes/api/auth.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Bring in Models & Helpers
+const User = require('../../models/user');
+const keys = require('../../config/keys');
+const { EMAIL_PROVIDER } = require('../../constants');
+
+const { secret, tokenLife } = keys.jwt;
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res
+        .status(400)
+        .json({ error: 'You must enter an email address.' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'You must enter a password.' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .send({ error: 'No user found for this email address.' });
+    }
+
+    if (user && user.provider !== EMAIL_PROVIDER.Email) {
+      return res.status(400).send({
+        error: `That email address is already in use using ${user.provider} provider.`
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        error: 'Password Incorrect'
+      });
+    }
+
+    const payload = {
+      id: user.id
+    };
+
+    const token = jwt.sign(payload, secret, { expiresIn: tokenLife });
+
+    if (!token) {
+      throw new Error();
+    }
+
+    res.status(200).json({
+      success: true,
+      token: `Bearer ${token}`,
+      user: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, firstName, lastName, password } = req.body;
+
+    if (!email) {
+      return res
+        .status(400)
+        .json({ error: 'You must enter an email address.' });
+    }
+
+    if (!firstName || !lastName) {
+      return res.status(400).json({ error: 'You must enter your full name.' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'You must enter a password.' });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: 'That email address is already in use.' });
+    }
+
+    const user = new User({
+      email,
+      password,
+      firstName,
+      lastName
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+    const registeredUser = await user.save();
+
+    const payload = {
+      id: registeredUser.id
+    };
+
+    const token = jwt.sign(payload, secret, { expiresIn: tokenLife });
+
+    res.status(200).json({
+      success: true,
+      token: `Bearer ${token}`,
+      user: {
+        id: registeredUser.id,
+        firstName: registeredUser.firstName,
+        lastName: registeredUser.lastName,
+        email: registeredUser.email,
+        role: registeredUser.role
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: 'Your request could not be processed. Please try again.'
+    });
+  }
+});
+
+module.exports = router;
